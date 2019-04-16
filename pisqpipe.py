2715a3a67007952af1e7a348c4c253dc00e08ead
Zhongxia Yan <zxyan@berkeley.edu>
2019-04-16 04:22:49 -0400

    proxy to forward commands to remote port

--- pisqpipe.py
+++ pisqpipe.py
@@ -2,28 +2,15 @@ import sys
 import socket
 import threading
 
+import win32console
+import pywintypes
+
 ADDR = 'localhost'
 SEND_PORT = 8082
 RECV_PORT = 8083
 
-# win32process.beginthreadex(None, 0, threadLoop, (), 0)
-# def threadLoop():
-# 	"""main function for the working thread"""
-# 	while True:
-# 		win32event.WaitForSingleObject(event1, win32event.INFINITE)
-# 		brain_turn()
-# 		win32event.SetEvent(event2)
-
-import win32api
-import win32event
-import win32console
-import win32process
-import pywintypes
-
-def pipeOut(what):
-	"""write a line to sys.stdout"""
-	ret = len(what)
-	print(what)
+def pipe_out(msg):
+	print(msg)
 	sys.stdout.flush()
 
 def get_line():
@@ -33,33 +20,34 @@ def listen():
 	sock_recv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock_recv.bind((ADDR, RECV_PORT))
 	sock_recv.listen(1)
-	print('Listening to %s %s' % (ADDR, RECV_PORT))
+	# print('Listening to %s %s' % (ADDR, RECV_PORT))
 	
 	conn, addr = sock_recv.accept()
-	print('Accepted connection from %s %s' % (ADDR, RECV_PORT))
+	# print('Accepted connection from %s %s' % (ADDR, RECV_PORT))
 	while True:
 		data = conn.recv(4096)
-		print(data)
+		if not data:
+			break
+		pipe_out(data.decode())
 
 def main():
-	"""main function for AI console application"""
-	#
 	handle = win32console.GetStdHandle(win32console.STD_INPUT_HANDLE)
 	try:
 		if handle.GetConsoleMode():
-			pipeOut("MESSAGE Gomoku AI should not be started directly. Please install gomoku manager (http://sourceforge.net/projects/piskvork). Then enter path to this exe file in players settings.")
+			pipe_out("MESSAGE Gomoku AI should not be started directly. Please install gomoku manager (http://sourceforge.net/projects/piskvork). Then enter path to this exe file in players settings.")
 	except pywintypes.error:
 		pass
-	
+
 	t_recv = threading.Thread(target=listen)
 	t_recv.start()
 
 	sock_send = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock_send.connect((ADDR, SEND_PORT))
+	# print('Sending to %s %s' % (ADDR, SEND_PORT))
 
 	while True:
 		line = get_line()
-		print(line)
-		sock.sendall(line)
+		sock_send.sendall(line.encode('utf-8'))
 
-main()
+if __name__ == '__main__':
+	main()
